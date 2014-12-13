3749f62018c709b8f3e1a5508fbdecf3426023db
HybridDog <adresse_test@t-online.de>
2014-12-13 22:34:27 +0100

    init

---
+++ init.lua
@@ -0,0 +1,48 @@
+local technic_available = rawget(_G, "technic") and true
+
+for i = 1,9 do
+	local scale = 3^i
+	local amount = scale^3
+	local ns = scale/2
+	scale = 1/scale
+	local name = "compressed_cobble:cobble_"..i
+	minetest.register_node(name, {
+		description = amount.." compressed cobble",
+		drawtype = "nodebox",
+		tiles = {"default_cobble.png"},
+		paramtype = "light",
+		groups = {cracky=3,oddly_breakable_by_hand=3},
+		sounds = default.node_sound_stone_defaults(),
+		visual_scale = scale,
+		wield_scale = scale,
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-ns, -ns, -ns, ns, ns, ns},
+			},
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
+			},
+		},
+		collision_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
+			},
+		},
+	})
+
+	if technic_available then
+		local input_i = i-1
+		local input
+		if input_i == 0 then
+			input = "default:cobble"
+		else
+			input = "compressed_cobble:cobble_"..input_i
+		end
+		technic.register_compressor_recipe({input = {input}, output = name})
+	end
+end
